6bec3538b58c62e86fcc72c3653662531cc4343b
James P Bondeson <jpbond58@gmail.com>
2020-08-11 16:31:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,9 @@ def adding_matz
 # return the entire updated hash
 programmer_hash[:yukihiro_matsumoto] << {
 		:known_for => "Ruby",
-		:languages => ["LISP", "C"]}, 
+		:languages => ["LISP", "C"]},
+programmer_hash
+
 	programmer_hash =
  		{
       :grace_hopper => {
